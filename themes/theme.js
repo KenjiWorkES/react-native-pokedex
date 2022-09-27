1959cbc67342f7ddabdb303c223d21070b7cd328
KenjiWorkES <lucas.kenjies@gmail.com>
2022-09-27 15:06:41 -0300

    add theme file with colors

---
+++ themes/theme.js
@@ -0,0 +1,84 @@
+export const theme = {
+  colors: {
+    /*basic colors*/
+    lightGray: '#f5f5f5',
+    darkGray: '#303943',
+    white: '#fff',
+    whiteIce: '#f4f5f4',
+
+    /*categories colors*/
+    lightGreen: '#46d7ab',
+    darkGreen: '#4fc1a6',
+    lightRed: '#fc7f63',
+    darkRed: '#fa6555',
+    lightBlue: '#6dc1fd',
+    darkBlue: '#64a9fa',
+    lightYellow: '#ffce4b',
+    darkYellow: '#f6c747',
+    lightPurple: '#9f5bba',
+    darkPurple: '#7c538c',
+    lightBrown: '#ca8179',
+    darkBrown: '#b1736c',
+    deepPurple: '#6c79db',
+
+    // PokemonType Background Colors
+
+    bugType1: '#1c4b27',
+    bugType2: '#3b9950',
+
+    dragonType1: '#61cad9',
+    dragonType2: '#61cad9',
+
+    fairyType1: '#971944',
+    fairyType2: '#ea1369',
+
+    fightingType1: '#994025',
+    fightingType2: '#ef6138',
+
+    flyingType1: '#e6e6e6',
+    flyingType2: '#4a677d',
+
+    ghostType1: '#906790',
+    ghostType2: '#33336b',
+
+    groundType1: '#a96f2e',
+    groundType2: '#6e491f',
+
+    iceType1: '#d8f0fa',
+    iceType2: '#dbf1fd',
+    iceType3: '#93d5f4',
+
+    normalType1: '#75515b',
+    normalType2: '#ae9397',
+
+    poisonType1: '#9b69d9',
+    poisonType2: '#5c2e87',
+    poisonType3: '#7c538c',
+
+    rockType1: '#8c3c24',
+    rockType2: '#48180b',
+
+    steelType1: '#42bd94',
+    steelType2: '#5f766e',
+
+    grassType1: '#147b3d',
+    grassType2: '#27cb4f',
+    grassType3: '#48d0b0',
+
+    waterType1: '#86a8fc',
+    waterType2: '#1552e1',
+    waterType3: '#77bdfe',
+
+    fireType1: '#fd4c5a',
+    fireType2: '#a92023',
+    fireType3: '#fb6c6c',
+
+    electricType1: '#fbcd66',
+
+    psychicType1: '#a42a6c',
+    psychicType2: '#f81c91',
+
+    darkType1: '#040706',
+    darkType2: '#282735',
+  },
+};
